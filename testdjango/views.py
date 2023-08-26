add3bd67fe4dfa47f7b7ab97753b74f2040da4bf
sandjonyves <sandjonyves@gmail.com>
2023-08-26 21:01:04 +0200

    test506

--- testdjango/views.py
+++ testdjango/views.py
@@ -2,4 +2,4 @@ from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 
 
-index = never_cache(TemplateView.as_view(template_name = 'build/index.html'))
+index = never_cache(TemplateView.as_view(template_name = 'index1.html'))
